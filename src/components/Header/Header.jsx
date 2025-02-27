6275da715751263937091ce64b36f048b7c4b9d4
Purushottam Goel <purushottamgoel@Purushottams-MacBook-Air.local>
2025-02-27 17:05:24 +0530

    Adding Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import chefClaudeLogo from "../../assets/chef-claude-icon.png"
+
+function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo} />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
+
+export default Header;
